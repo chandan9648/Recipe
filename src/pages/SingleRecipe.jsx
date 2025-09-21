92f3312632c51321d64cff83cfd94b0cddbc011e
chandan9648 <chandankkumar156@gmail.com>
2025-09-21 13:58:45 +0530

    Implement single recipe view with data fetching and loading state

--- src/pages/SingleRecipe.jsx
+++ src/pages/SingleRecipe.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { recipecontext } from '../context/RecipeContext';
 
 const SingleRecipe = () => {
-  return (
-    <div>
-        SingleRecipe
+  const {data} = useContext(recipecontext);
+  const params = useParams();
+  const recipe = data.find((recipe) => parseInt(params.id) === recipe.id);
+  console.log(recipe);
+
+    return recipe ? (
+    <div className='flex w-full'>
+      <div className='left w-1/2 p-2'>
+        <h1 className='text-4xl font-black'>{recipe.title}</h1>
+        <img className='h-[20vh]' src={recipe.image} alt="image"/>
+      </div>
+     <div className='right w-1/2 p-2'></div>
     </div>
-  )
-}
+      ) : (
+        "Loading..."
+      );
+  };
 
 export default SingleRecipe;
