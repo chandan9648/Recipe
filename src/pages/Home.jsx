9304c752964bf07e37e99caa7d977c91a1cfb5f9
chandan9648 <chandankkumar156@gmail.com>
2025-09-22 16:53:32 +0530

    Add axios instance with interceptors and update Home component to fetch products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
+import { useEffect } from "react";
+import axios from "../utils/axios";
+
 const Home = () => {
+
+  const getproduct = async () =>{
+    try{
+      const response = await axios.get('/products');
+      console.log("response",response.data);
+    }catch(err){
+      console.log("error",err);
+    }
+  }
+  //to carry out side effects in functional components
+  useEffect(() => {
+    getproduct();
+  }, []);   /*[] it stop infinite loop by running only once when component mounts*/
+
   return (
     <div>
-      Home
+      <h1>Home Page</h1>
+    
     </div>
   )
-}
 
-export default Home
+
+};
+
+export default Home;
